d6f3455ed3b9d29d0820b7a11ba0ee15f2e04acd
unknown <danielhernandezmontero8@gmail.com>
2024-11-26 16:58:31 +0100

    first commit

---
+++ src/main/java/com/helpinghandslocation/helpinghandslocation/HelpinghandslocationApplication.java
@@ -0,0 +1,13 @@
+package com.helpinghandslocation.helpinghandslocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpinghandslocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpinghandslocationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helpinghandslocation/helpinghandslocation/HelpinghandslocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helpinghandslocation.helpinghandslocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpinghandslocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
